f2cdfb56e460940ee0fab4fd131071a8590e6b18
GuiltyKing12 <bher@mines.edu>
2016-12-05 19:35:46 -0700

    Reorganzied the files and seperated headers from cpp

--- src/Planet.cpp
+++ src/Planet.cpp
@@ -1,7 +1,24 @@
 #include "Planet.h"
+// Very important!!!
+// GLEW MUST BE INCLUDED BEFORE gl.h is included
+#include <GL/glew.h>		// OpenGL Extension Wrangler
 
+// Include our OpenGL & GLUT libraries
+// GLUT automatically includes glu.h and gl.h
+#ifdef __APPLE__
+#include <GLUT/glut.h>	// GLUT, GLU, GL Libraries for Mac
+#include <SOIL.h>  	// Simple OpenGL Image Library
+#else
+#include <GL/glut.h>	// GLUT, GLU, GL Libraries for Windows or *nix
+#include <SOIL/SOIL.h>  	// Simple OpenGL Image Library
+#endif
 void Planet::draw() {
-    
+    glPushMatrix();
+    {
+        glTranslatef(position.getX(), position.getY(), position.getZ());
+        glutSolidSphere(planetRadius, 30, 30);
+    }
+    glPopMatrix();
 }
 
 void Planet::update() {
